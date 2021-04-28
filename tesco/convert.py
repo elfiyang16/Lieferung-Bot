7d48f591e0b90338ebb386839aac5279da8e4287
Elfi_Y <elfiyang16@gmail.com>
2021-04-28 22:00:19 +0100

    refactor: on bulk save pdf

--- tesco/convert.py
+++ tesco/convert.py
@@ -2,7 +2,7 @@ from PIL import Image
 import pathlib
 
 GROCERY_SHOP = "tesco"
-# image_files = [x for x in image_paths if x.is_file()]
+# img_files = [x for x in img_paths if x.is_file()]
 
 
 class Convertor:
@@ -13,23 +13,33 @@ class Convertor:
         img_path = pathlib.Path.cwd()/"screenshots"
         pdf_path = pathlib.Path.cwd()/"pdfs"
         grocery_screenshots_dir = img_path/grocery_shop
-        self.grocery_pdfs_dir = pdf_path/grocery_shop
-        image_paths = pathlib.Path(grocery_screenshots_dir).rglob("*.png")
-        self.image_files = list(filter(pathlib.Path.is_file, image_paths))
+        grocery_pdfs_dir = pdf_path/grocery_shop
+        img_paths = pathlib.Path(grocery_screenshots_dir).rglob("*.png")
+        self.img_file_paths = list(filter(pathlib.Path.is_file, img_paths))
+        # using the first file as out filename
+        filename = self.get_filename(self.img_file_paths[0])
+        self.out_path = grocery_pdfs_dir/f"{filename}.pdf"
+        self.converted_imgs = []
 
     def convert_pdf(self):
         """
-        convert a list of images based on its filepath into pdf,
+        convert a list of imgs based on its filepath into pdf,
         and save to designated location
         """
-        for img_path in self.image_files:
-            filename = self.get_filename(img_path)
+        for img_path in self.img_file_paths:
             converted_img = Image.open(img_path).convert('RGB')
-            converted_img.save(self.grocery_pdfs_dir/f"{filename}.pdf")
+            self.converted_imgs.append(converted_img)
+
+    def save_pdf(self):
+        """
+        save a list of converted pdf (binary) to file
+        """
+        self.converted_imgs[0].save(
+            self.out_path, save_all=True, quality=100, append_images=self.converted_imgs[1:])
 
     def get_filename(self, img_filepath):
         """
-        get the pdf filename based on image file name 
+        get the pdf filename based on img file name 
         e.g. tesco img file:tesco-Apr 28 - May 04
         pdf file:
         """
@@ -39,3 +49,4 @@ class Convertor:
 if __name__ == "__main__":
     convertor = Convertor(GROCERY_SHOP)
     convertor.convert_pdf()
+    convertor.save_pdf()
